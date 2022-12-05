4a0b26583eeec67cf9d3f3bdb874bfc1183a689d
rdz <rdszhao@gmail.com>
2022-12-04 16:39:13 -0800

    added main

--- strava.py
+++ strava.py
@@ -7,42 +7,41 @@ import streamlit as st
 import sweat
 from bokeh.models.widgets import Div
 
-
-APP_URL = os.environ["APP_URL"]
-STRAVA_CLIENT_ID = os.environ["STRAVA_CLIENT_ID"]
-STRAVA_CLIENT_SECRET = os.environ["STRAVA_CLIENT_SECRET"]
-STRAVA_AUTHORIZATION_URL = "https://www.strava.com/oauth/authorize"
-STRAVA_API_BASE_URL = "https://www.strava.com/api/v3"
-DEFAULT_ACTIVITY_LABEL = "NO_ACTIVITY_SELECTED"
-STRAVA_ORANGE = "#fc4c02"
-
+APP_URL = os.environ['APP_URL']
+STRAVA_CLIENT_ID = os.environ['STRAVA_CLIENT_ID']
+STRAVA_CLIENT_SECRET = os.environ['STRAVA_CLIENT_SECRET']
+STRAVA_AUTHORIZATION_URL = 'https://www.strava.com/oauth/authorize'
+STRAVA_API_BASE_URL = 'https://www.strava.com/api/v3'
+DEFAULT_ACTIVITY_LABEL = 'NO_ACTIVITY_SELECTED'
+STRAVA_ORANGE = '#fc4c02'
 
 
+# load images
 @st.cache(show_spinner=False)
 def load_image_as_base64(image_path):
-    with open(image_path, "rb") as f:
+    with open(image_path, 'rb') as f:
         contents = f.read()
-    return base64.b64encode(contents).decode("utf-8")
+    return base64.b64encode(contents).decode('utf-8')
 
-
+# loads strava in markdown compatible format
 def powered_by_strava_logo():
-    base64_image = load_image_as_base64("./static/api_logo_pwrdBy_strava_horiz_light.png")
+    base64_image = load_image_as_base64('./static/api_logo_pwrdBy_strava_horiz_light.png')
     st.markdown(
-        f'<img src="data:image/png;base64,{base64_image}" width="100%" alt="powered by strava">',
+        f'<img src='data:image/png;base64,{base64_image}' width='100%' alt='powered by strava'>',
         unsafe_allow_html=True,
     )
 
 
 def authorization_url():
     request = httpx.Request(
-        method="GET",
+        method='GET',
         url=STRAVA_AUTHORIZATION_URL,
         params={
-            "client_id": STRAVA_CLIENT_ID,
-            "redirect_uri": APP_URL,
-            "response_type": "code",
-            "approval_prompt": "auto",
-            "scope": "activity:read_all"
+            'client_id': STRAVA_CLIENT_ID,
+            'redirect_uri': APP_URL,
+            'response_type': 'code',
+            'approval_prompt': 'auto',
+            'scope': 'activity:read_all'
         }
     )
 
@@ -61,12 +60,12 @@ def login_header(header=None):
     with col1:
         powered_by_strava_logo()
 
-    base64_image = load_image_as_base64("./static/btn_strava_connectwith_orange@2x.png")
+    base64_image = load_image_as_base64('./static/btn_strava_connectwith_orange@2x.png')
     base.markdown(
         (
-            f"<a href=\"{strava_authorization_url}\">"
-            f"  <img alt=\"strava login\" src=\"data:image/png;base64,{base64_image}\" width=\"100%\">"
-            f"</a>"
+            f'<a href=\'{strava_authorization_url}\'>'
+            f'  <img alt=\'strava login\' src=\'data:image/png;base64,{base64_image}\' width=\'100%\'>'
+            f'</a>'
         ),
         unsafe_allow_html=True,
     )
@@ -83,9 +82,9 @@ def logout_header(header=None):
     with col2:
         powered_by_strava_logo()
 
-    if base.button("Log out"):
-        js = f"window.location.href = '{APP_URL}'"
-        html = f"<img src onerror=\"{js}\">"
+    if base.button('Log out'):
+        js = f'window.location.href = '{APP_URL}''
+        html = f'<img src onerror=\'{js}\'>'
         div = Div(text=html)
         st.bokeh_chart(div)
 
@@ -97,26 +96,26 @@ def logged_in_title(strava_auth, header=None):
         col, _, _, _ = header
         base = col
 
-    first_name = strava_auth["athlete"]["firstname"]
-    last_name = strava_auth["athlete"]["lastname"]
-    col.markdown(f"*Welcome, {first_name} {last_name}!*")
+    first_name = strava_auth['athlete']['firstname']
+    last_name = strava_auth['athlete']['lastname']
+    col.markdown(f'*Welcome, {first_name} {last_name}!*')
 
 
 @st.cache(show_spinner=False, suppress_st_warning=True)
 def exchange_authorization_code(authorization_code):
     response = httpx.post(
-        url="https://www.strava.com/oauth/token",
+        url='https://www.strava.com/oauth/token',
         json={
-            "client_id": STRAVA_CLIENT_ID,
-            "client_secret": STRAVA_CLIENT_SECRET,
-            "code": authorization_code,
-            "grant_type": "authorization_code",
+            'client_id': STRAVA_CLIENT_ID,
+            'client_secret': STRAVA_CLIENT_SECRET,
+            'code': authorization_code,
+            'grant_type': 'authorization_code',
         }
     )
     try:
         response.raise_for_status()
     except httpx.HTTPStatusError:
-        st.error("Something went wrong while authenticating with Strava. Please reload and try again")
+        st.error('Something went wrong while authenticating with Strava. Please reload and try again')
         st.experimental_set_query_params()
         st.stop()
         return
@@ -128,10 +127,10 @@ def exchange_authorization_code(authorization_code):
 
 def authenticate(header=None, stop_if_unauthenticated=True):
     query_params = st.experimental_get_query_params()
-    authorization_code = query_params.get("code", [None])[0]
+    authorization_code = query_params.get('code', [None])[0]
 
     if authorization_code is None:
-        authorization_code = query_params.get("session", [None])[0]
+        authorization_code = query_params.get('session', [None])[0]
 
     if authorization_code is None:
         login_header(header=header)
@@ -158,14 +157,14 @@ def header():
 
 @st.cache(show_spinner=False)
 def get_activities(auth, page=1):
-    access_token = auth["access_token"]
+    access_token = auth['access_token']
     response = httpx.get(
-        url=f"{STRAVA_API_BASE_URL}/athlete/activities",
+        url=f'{STRAVA_API_BASE_URL}/athlete/activities',
         params={
-            "page": page,
+            'page': page,
         },
         headers={
-            "Authorization": f"Bearer {access_token}",
+            'Authorization': f'Bearer {access_token}',
         },
     )
 
@@ -173,47 +172,47 @@ def get_activities(auth, page=1):
 
 
 def activity_label(activity):
-    if activity["name"] == DEFAULT_ACTIVITY_LABEL:
-        return ""
+    if activity['name'] == DEFAULT_ACTIVITY_LABEL:
+        return ''
 
-    start_date = arrow.get(activity["start_date_local"])
-    human_readable_date = start_date.humanize(granularity=["day"])
-    date_string = start_date.format("YYYY-MM-DD")
+    start_date = arrow.get(activity['start_date_local'])
+    human_readable_date = start_date.humanize(granularity=['day'])
+    date_string = start_date.format('YYYY-MM-DD')
 
-    return f"{activity['name']} - {date_string} ({human_readable_date})"
+    return f'{activity['name']} - {date_string} ({human_readable_date})'
 
 
 def select_strava_activity(auth):
     col1, col2 = st.beta_columns([1, 3])
     with col1:
         page = st.number_input(
-            label="Activities page",
+            label='Activities page',
             min_value=1,
-            help="The Strava API returns your activities in chunks of 30. Increment this field to go to the next page.",
+            help='The Strava API returns your activities in chunks of 30. Increment this field to go to the next page.',
         )
 
     with col2:
         activities = get_activities(auth=auth, page=page)
         if not activities:
-            st.info("This Strava account has no activities or you ran out of pages.")
+            st.info('This Strava account has no activities or you ran out of pages.')
             st.stop()
-        default_activity = {"name": DEFAULT_ACTIVITY_LABEL, "start_date_local": ""}
+        default_activity = {'name': DEFAULT_ACTIVITY_LABEL, 'start_date_local': ''}
 
         activity = st.selectbox(
-            label="Select an activity",
+            label='Select an activity',
             options=[default_activity] + activities,
             format_func=activity_label,
         )
 
-    if activity["name"] == DEFAULT_ACTIVITY_LABEL:
-        st.write("No activity selected")
+    if activity['name'] == DEFAULT_ACTIVITY_LABEL:
+        st.write('No activity selected')
         st.stop()
         return
 
-    activity_url = f"https://www.strava.com/activities/{activity['id']}"
+    activity_url = f'https://www.strava.com/activities/{activity['id']}'
         
     st.markdown(
-        f"<a href=\"{activity_url}\" style=\"color:{STRAVA_ORANGE};\">View on Strava</a>",
+        f'<a href=\'{activity_url}\' style=\'color:{STRAVA_ORANGE};\'>View on Strava</a>',
         unsafe_allow_html=True
     )
 
@@ -223,5 +222,5 @@ def select_strava_activity(auth):
 
 @st.cache(show_spinner=False, max_entries=30, allow_output_mutation=True)
 def download_activity(activity, strava_auth):
-    with st.spinner(f"Downloading activity \"{activity['name']}\"..."):
-        return sweat.read_strava(activity["id"], strava_auth["access_token"])
+    with st.spinner(f'Downloading activity \'{activity['name']}\'...'):
+        return sweat.read_strava(activity['id'], strava_auth['access_token'])
