f42962151e65391dc83378489255366039577113
rdz <rdszhao@gmail.com>
2022-12-06 16:44:07 -0800

    added debug func

---
+++ forms.py
@@ -0,0 +1,20 @@
+from requests import post
+
+def submit(name, email, craft, activity, debug=False):
+	distance = round(float(activity['distance']) / 1609, 2)
+	year, month, day= activity['start_date'].split('T')[0].split('-')
+	link = f"https://www.strava.com/activities/{activity['id']}"
+	values = [email, month, day, year, name, distance, 'mi', craft link]
+
+	real_keys = ['emailAddress', 'entry.356064094_month', 'entry.356064094_day', 'entry.356064094_year', 'entry.62320042', 'entry.773830580', 'entry.1113937566', 'entry.140974962', 'entry.1879621063']
+	debug_keys = ['entry.565666177', 'entry.1161307666_month', 'entry.1161307666_day', 'entry.1161307666_year', 'entry.1564166327', 'entry.652460081', 'entry.226956376', 'entry.1024488085', 'entry.136322056']
+	real_url = 'https://docs.google.com/forms/d/e/1FAIpQLSeYnetUQ5T1BHD5JHm3TNZ6myEgEvP2JwfJD_Sa16l7xVeaAg/'
+	debug_url = 'https://docs.google.com/forms/d/e/1FAIpQLSdMf1jraG5M9v4JYUlzH69V9Y5QzPQzdz0a39X_ia4gHtJW7Q/'
+
+	if debug:
+		url = f"{debug_url}/formResponse"
+		entries = dict(zip(debug_keys, values))
+	else:
+		url = f"{real_url}/formResponse"
+		entries = dict(zip(real_keys, values))
+	post(url, data=entries)
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import base64
-import re
-import altair as alt
 import streamlit as st
-import strava
-from pandas.api.types import is_numeric_dtype
+from re import compile, fullmatch
+from strava import header, authenticate, select_strava_activity
+from forms import submit
     
 
 if __name__ == '__main__':
@@ -14,7 +12,7 @@ if __name__ == '__main__':
 
     st.image("https://analytics.gssns.io/pixel.png")
 
-    strava_header = strava.header()
+    strava_header = header()
 
     st.markdown(
         """
@@ -24,7 +22,7 @@ if __name__ == '__main__':
         """
     )
 
-    strava_auth = strava.authenticate(header=strava_header, stop_if_unauthenticated=False)
+    strava_auth = authenticate(header=strava_header, stop_if_unauthenticated=False)
 
     if strava_auth is None:
         st.markdown("Click the \"Connect with Strava\" button at the top to login with your Strava account and get started.")
@@ -38,14 +36,14 @@ if __name__ == '__main__':
     with st.form('email'):
             email = st.text_input('email')
             saved_email = st.form_submit_button('save')
-            regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
-            valid_email = re.fullmatch(regex, email)
+            regex = compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+            valid_email = fullmatch(regex, email)
     if valid_email:
         st.write('email saved')
     else:
         st.write('please enter a valid email')
 
-    activity = strava.select_strava_activity(strava_auth)
+    activity = select_strava_activity(strava_auth)
 
     crafts = [
         'dragon boat',
@@ -64,8 +62,6 @@ if __name__ == '__main__':
         craft = st.selectbox('Craft', crafts)
         submitted = st.form_submit_button('submit')
     if submitted:
+        name = f"{strava_auth['athlete']['firstname']} {strava_auth['athlete']['lastname']}"
+        submit(name, email, craft, activity, debug=True)
         st.write('activity submitted')
-        distance = round(float(activity['distance']) / 1609, 2)
-        yy, mm, dd = activity['start_date'].split('T')[0].split('-')
-        activity_url = f"https://www.strava.com/activities/{activity['id']}"
-        print(email, craft, distance, mm, dd, yy, '\n', activity_url)
