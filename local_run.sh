4cbb58836a761af03c18f7772e2ccb8567599b3b
rdz <rdszhao@gmail.com>
2023-01-07 20:54:46 -0800

    fixed env issues

---
+++ local_run.sh
@@ -0,0 +1,1 @@
+streamlit run --server.enableCORS false --server.port=8051 --server.address=localhost main.py
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-streamlit run --server.enableCORS false --server.port=8051 --server.address=localhost main.py
+set - a
+source .env
+set +a
+streamlit run --server.enableCORS=false --server.port=8051 --server.address=localhost main.py
